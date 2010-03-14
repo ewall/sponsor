5867eaeb81c41e8fd96ff92478d5c5c7d8080e60
ewall <e@ewall.org>
2010-03-14 15:17:18 -0400

    completed config of Authlogic

--- config/routes.rb
+++ config/routes.rb
@@ -31,13 +31,21 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "user_sessions"
 
   # See how all your routes lay out with "rake routes"
+
+	
+	# to support Authlogic
+	map.resources :users
+	map.login 'login', :controller => 'user_sessions', :action => 'new'  
+	map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'  
+	map.resources :user_sessions
+
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
+	map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
