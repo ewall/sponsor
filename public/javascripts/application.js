41c5e8e0b92f0fb5448ed439f1499e805c69321a
ewall <e@ewall.org>
2010-03-29 01:37:26 -0400

    AJAX history works, not sure if I like it

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,26 @@
-// AJAX pagination support via jQuery
+// AJAX pagination support via jQuery; history fix via jQuery-BBQ
+
+baseurl = location.protocol + '//' + location.host + location.pathname;
+
 $(function() {
-  $('.pagination a').live('click', function(e) {
-    $(this).parent('.pagination')
-           .append("<img src='/images/spinner.gif' class='spinner' />");
-		//$.get(this.href, null, null, 'script');
-		$.getScript(this.href);
-    return false;
+  $('.pagination a').live('click', function(e) { 
+    params = $.deparam.querystring(this.href);
+		new_url = $.param.fragment(baseurl, params, 2);
+		$(this).parent('.pagination').append("<img src='/images/spinner.gif' class='spinner' />"); // show loading spinner
+		if (params.page==1) {
+			location.hash = '' // no need for hash if page=1
+		} else {
+			location.hash = $.param.fragment( new_url ); // set the new hash, which will trigger the hashchange event
+		}
+		return false;
   });
 });
+
+$(window).bind( 'hashchange', function(e) {
+	params = e.fragment; // pre-jQuery1.4: $.deparam.fragment(document.location.href);
+	$.getScript( $.param.querystring(baseurl, params) );
+});
+
+if (location.hash) {
+  $(window).trigger( 'hashchange' );
+}
